b8daaf005f5a40a112a928c9725ec3277a01267d
sil <git@sil.dev>
2023-01-09 20:13:46 -0600

    Removed unused hooks

--- schema/sh_schema.lua
+++ schema/sh_schema.lua
@@ -2,9 +2,5 @@ Schema.name = "Palomino Roleplay"
 Schema.author = "Palomino Roleplay"
 Schema.description = "A player-first roleplay experience."
 
-ix.util.Include("cl_hooks.lua")
-ix.util.Include("sh_hooks.lua")
-ix.util.Include("sv_hooks.lua")
-
 ix.util.Include("meta/sh_character.lua")
 ix.util.Include("meta/sh_player.lua")
