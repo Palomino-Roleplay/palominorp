f30d44f317236a659e4e1efde02a24f78e05b1fe
sil <git@sil.dev>
2023-01-19 03:37:39 -0600

    Include cl_schema.lua, set weapon to always raised by default

--- schema/sh_schema.lua
+++ schema/sh_schema.lua
@@ -2,5 +2,11 @@ Schema.name = "Palomino Roleplay"
 Schema.author = "Palomino Roleplay"
 Schema.description = "A player-first roleplay experience."
 
+ix.util.Include("cl_schema.lua")
+
 ix.util.Include("meta/sh_character.lua")
 ix.util.Include("meta/sh_player.lua")
+
+-- Default config values
+
+ix.config.SetDefault( "weaponAlwaysRaised", true )
