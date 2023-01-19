839d30dd34e789bf6e2fcd5c1ba10ac52094d654
sil <git@sil.dev>
2023-01-19 03:36:56 -0600

    Added fancy schmancy print function

---
+++ schema/libs/sh_dev.lua
@@ -0,0 +1,20 @@
+function Print( ... )
+    local tArgs = { ... }
+
+    if not tArgs then return end
+
+    local sText = ""
+    for n = 1, #tArgs do
+        if istable(tArgs[n]) then
+            tArgs[n] = table.ToString( tArgs[n], "Table " .. n .. ":", true )
+        end
+
+        sText = sText .. tostring( tArgs[n] )
+    end
+
+    MsgC(
+        Color( 122, 236, 202),
+        sText,
+        "\n"
+    )
+end
