31bf94898bdfc0afa59b0067ed096627db7d1129
sil <git@sil.dev>
2023-01-19 03:40:18 -0600

    DarkRP plugin: wrapper library for darkrp addons

---
+++ plugins/darkrp/meta/sh_character.lua
@@ -0,0 +1,9 @@
+local PLY = FindMetaTable("Player")
+
+function PLY:IsCP()
+    return self:GetCharacter():IsPolice()
+end
+
+function PLY:IsArrested()
+    return self:GetCharacter():IsArrested()
+end
