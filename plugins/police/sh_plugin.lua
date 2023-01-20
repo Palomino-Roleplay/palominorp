56c64b0e105b8f4d9453a6d4fa94816d33e647ff
sil <git@sil.dev>
2023-01-20 12:49:24 -0600

    Added arrest command, included hooks files

--- plugins/police/sh_plugin.lua
+++ plugins/police/sh_plugin.lua
@@ -6,9 +6,26 @@ PLUGIN.description = "Adds basic police functionality."
 
 ix.util.Include( "sh_arrest.lua" )
 
-ix.util.Include( "meta/sh_character.lua" )
 ix.util.Include( "meta/sv_character.lua" )
+ix.util.Include( "meta/sh_character.lua" )
+
+ix.util.Include( "hooks/sv_hooks.lua" )
+ix.util.Include( "hooks/sh_hooks.lua" )
+ix.util.Include( "hooks/cl_hooks.lua" )
 
 PLUGIN.PrisonPositions = {
     Vector( 2940, 2203, 144 + 16 ),
 }
+
+ix.command.Add("Arrest", {
+    description = "Arrests a character and puts them in jail.",
+    adminOnly = true,
+    arguments = {
+        ix.type.character,
+        ix.type.number,
+        ix.type.text
+    },
+    OnRun = function( self, pAdmin, target, minutes, reason )
+        target:Arrest( nil, minutes * 60, reason )
+    end
+} )
