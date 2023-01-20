6ab5f3569b5d9e493b0b39c9433b319b8d0d2a09
sil <git@sil.dev>
2023-01-20 13:30:47 -0600

    Removed unused include

--- plugins/police/sh_plugin.lua
+++ plugins/police/sh_plugin.lua
@@ -4,8 +4,6 @@ PLUGIN.name = "Police"
 PLUGIN.author = "sil"
 PLUGIN.description = "Adds basic police functionality."
 
-ix.util.Include( "sh_arrest.lua" )
-
 ix.util.Include( "meta/sv_character.lua" )
 ix.util.Include( "meta/sh_character.lua" )
 
