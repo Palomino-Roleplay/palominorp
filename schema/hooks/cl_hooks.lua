13b157b570836978c59a26ec3115eb06b43c5d48
sil <git@sil.dev>
2023-01-20 18:37:54 -0600

    Credits -> helix; don't display description in You panel

--- schema/hooks/cl_hooks.lua
+++ schema/hooks/cl_hooks.lua
@@ -4,9 +4,14 @@ end
 
 -- Can't do Schema:PopulateHelpMenu() because it's run before tTabs["plugins"] is added
 hook.Add( "PopulateHelpMenu", "Test.PopulateHelpMenu", function( tTabs )
+    tTabs["helix"] = function(container)
+        container:Add("ixCredits")
+    end
     tTabs["plugins"] = nil
 end )
+hook.Remove( "PopulateHelpMenu", "ixCredits" )
 
 function Schema:CanCreateCharacterInfo( tSuppress )
     tSuppress.attributes = true
+    tSuppress.description = true
 end
