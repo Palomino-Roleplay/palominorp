44d29c3ba1822621327526a7a681264000b08563
sil <git@sil.dev>
2023-01-21 19:54:41 -0600

    Disable zooming

--- schema/hooks/sh_hooks.lua
+++ schema/hooks/sh_hooks.lua
@@ -5,3 +5,8 @@ end
 function Schema:CanPlayerJoinClass()
     return false
 end
+
+function Schema:PlayerInitialSpawn( pPlayer )
+    -- @TODO: Move this to a better place
+    pPlayer:SetCanZoom( false )
+end
