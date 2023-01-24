c491e61a3457702f8a5009ff3cbb7028a5a10b4f
sil <git@sil.dev>
2023-01-24 16:12:40 -0600

    Added dev ent & swep

---
+++ plugins/prp_devtools/entities/weapons/prp_dev_swep.lua
@@ -0,0 +1,23 @@
+SWEP.PrintName              = "Dev Weapon"
+SWEP.Author                 = "sil"
+SWEP.Instructions           = "Left click to find out"
+SWEP.Category               = "Palomino"
+
+SWEP.Spawnable              = true
+SWEP.AdminOnly              = true
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo		    = "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		    = "none"
+
+SWEP.ViewModel			    = "models/weapons/c_rpg.mdl"
+SWEP.WorldModel			    = "models/weapons/w_rocket_launcher.mdl"
+SWEP.UseHands               = true
+
+SWEP.DrawAmmo               = false
