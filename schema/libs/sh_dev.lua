1059b32d41e92fdf144ab8e036f68844a6a36e82
sil <git@sil.dev>
2023-01-26 06:33:12 -0600

    Debug pos commands

--- schema/libs/sh_dev.lua
+++ schema/libs/sh_dev.lua
@@ -31,3 +31,37 @@ function Print( ... )
         "\n"
     )
 end
+
+if CLIENT then
+    concommand.Add( "prp_eyepos", function( pPlayer )
+        Print( "Vector( " .. math.Round( pPlayer:EyePos().x ) .. ", " .. math.Round( pPlayer:EyePos().y ) .. ", " .. math.Round( pPlayer:EyePos().z ) .. " )" )
+    end )
+
+    concommand.Add( "prp_getpos", function( pPlayer )
+        Print( "Vector( " .. math.Round( pPlayer:GetPos().x ) .. ", " .. math.Round( pPlayer:GetPos().y ) .. ", " .. math.Round( pPlayer:GetPos().z ) .. " )" )
+    end )
+
+    concommand.Add( "prp_getang", function( pPlayer )
+        Print( "Angle( " .. math.Round( pPlayer:GetAngles().p ) .. ", " .. math.Round( pPlayer:GetAngles().y ) .. ", " .. math.Round( pPlayer:GetAngles().r ) .. " )" )
+    end )
+
+    concommand.Add( "prp_getpos_trace", function()
+        local tr = util.TraceLine( {
+            start = LocalPlayer():GetShootPos(),
+            endpos = LocalPlayer():GetShootPos() + LocalPlayer():GetAimVector() * 10000,
+            filter = LocalPlayer()
+        } )
+
+        Print( "Vector( " .. math.Round( tr.HitPos.x ) .. ", " .. math.Round( tr.HitPos.y ) .. ", " .. math.Round( tr.HitPos.z ) .. " )" )
+    end )
+
+    concommand.Add( "prp_getang_trace", function()
+        local tr = util.TraceLine( {
+            start = LocalPlayer():GetShootPos(),
+            endpos = LocalPlayer():GetShootPos() + LocalPlayer():GetAimVector() * 10000,
+            filter = LocalPlayer()
+        } )
+
+        Print( "Angle( " .. math.Round( tr.HitNormal:Angle().p ) .. ", " .. math.Round( tr.HitNormal:Angle().y ) .. ", " .. math.Round( tr.HitNormal:Angle().r ) .. " )" )
+    end )
+end
