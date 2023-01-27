1c043b5a3fb07005ab24b3bd5523255f09494aa6
sil <git@sil.dev>
2023-01-27 02:32:04 -0600

    Stub tazer

---
+++ plugins/prp_police/entities/weapons/prp_tazer.lua
@@ -0,0 +1,24 @@
+SWEP.PrintName              = "Tazer"
+SWEP.Author                 = "sil"
+SWEP.Instructions           = "Left click to stun"
+SWEP.Category               = "Palomino: Police"
+
+SWEP.Spawnable              = true
+SWEP.AdminOnly              = true
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo		    = "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		    = "none"
+
+SWEP.ViewModel = Model("models/realistic_police/taser/c_taser.mdl")
+SWEP.WorldModel = Model("models/realistic_police/taser/w_taser.mdl")
+SWEP.ViewModelFOV = 60
+SWEP.UseHands = true
+
+SWEP.DrawAmmo               = false
