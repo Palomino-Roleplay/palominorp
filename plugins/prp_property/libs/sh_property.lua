9c4d515081a11dffdb272d9d5450e84e83ce0d80
sil <git@sil.dev>
2023-02-14 08:58:35 -0600

    Added rentable property accessor

--- plugins/prp_property/libs/sh_property.lua
+++ plugins/prp_property/libs/sh_property.lua
@@ -4,6 +4,10 @@ PRP.Property.List = PRP.Property.List or {}
 
 function PRP.Property.Register( oProperty )
     PRP.Property.List[oProperty:GetID()] = oProperty
+
+    if oProperty:GetRentable() then
+        PRP.Property.Rentable[oProperty:GetID()] = oProperty
+    end
 end
 
 function PRP.Property.Get( sProperty )
@@ -12,4 +16,8 @@ end
 
 function PRP.Property.GetAll()
     return PRP.Property.List
+end
+
+function PRP.Property.GetRentable()
+    return PRP.Property.Rentable
 end
