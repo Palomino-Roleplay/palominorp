cbdca2dba0939013d5a3c8d0f40456d1933ae48f
Knight <gatorguy333@gmail.com>
2023-07-13 20:49:15 -0600

    Added Primary Weapons: SKS and MP7 To Items

---
+++ schema/items/weapons/sh_mp7.lua
@@ -0,0 +1,7 @@
+ITEM.name = "MP7"
+ITEM.description = "2 Better Than The MP5"
+ITEM.model = "models/cw2/smgs/mp7_world.mdl"
+ITEM.class = "cw_mp7_official"
+ITEM.weaponCategory = "primary"
+ITEM.width = 2
+ITEM.height = 1
---
+++ schema/items/weapons/sh_sks.lua
@@ -0,0 +1,7 @@
+ITEM.name = "SKS"
+ITEM.description = "Poor Man's AK"
+ITEM.model = "models/weapons/w_rif_ak47.mdl"
+ITEM.class = "khr_sks"
+ITEM.weaponCategory = "primary"
+ITEM.width = 2
+ITEM.height = 1
