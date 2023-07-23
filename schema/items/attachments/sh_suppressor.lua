4a2687b7b62ee3a7d83db2fae8aafc933e27b9ce
sil <git@sil.dev>
2023-07-22 22:29:59 -0500

    Added Ruger Suppressor

---
+++ schema/items/attachments/sh_suppressor.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Ruger Suppressor"
+ITEM.model = "models/Items/BoxSRounds.mdl"
+ITEM.description = "A box containing a suppressor for the Ruger Mk3"
+ITEM.attachment = "md_rugersup"
+ITEM.width = 1
+ITEM.height = 1
