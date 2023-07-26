741e5c5067ca732bd65bb40db213ab2f58a46492
sil <git@sil.dev>
2023-07-25 20:56:48 -0500

    Added supported weapons to ruger suppressor item definition

--- schema/items/attachments/sh_suppressor.lua
+++ schema/items/attachments/sh_suppressor.lua
@@ -3,5 +3,8 @@ ITEM.name = "Ruger Suppressor"
 ITEM.model = "models/Items/BoxSRounds.mdl"
 ITEM.description = "A box containing a suppressor for the Ruger Mk3"
 ITEM.attachment = "md_rugersup"
+ITEM.weapons = {
+    ["khr_rugermk3"] = true
+}
 ITEM.width = 1
 ITEM.height = 1
