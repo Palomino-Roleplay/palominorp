391be4e17d9e209ced3afd4cc9c94c8d33514a1e
sil <git@sil.dev>
2023-08-12 00:38:54 -0500

    Improved radio sounds & display

--- schema/hooks/cl_hooks.lua
+++ schema/hooks/cl_hooks.lua
@@ -3,7 +3,33 @@ function Schema:BuildBusinessMenu()
 end
 
 function Schema:PlayerStartVoice( pPlayer )
+    -- if pPlayer == LocalPlayer() then return end
+    -- print("wtff!!!")
+    
+
+    -- if pPlayer == LocalPlayer() then
+    --     print("wtf???")
+    --     if not pPlayer._bInRadio then return true end
+    -- end
+
+    -- if LocalPlayer():OnSameChannel( pPlayer ) then
+    --     print( "On same channel" )
+    --     -- pPlayer._bInRadio = true
+    --     -- return true
+    -- else
+    --     print( "Not on same channel" )
+    --     -- pPlayer._bInRadio = false
+    --     -- return false
+    -- end
+
     if not LocalPlayer():Alive() then return true end
+    if LocalPlayer():OnSameChannel( pPlayer ) and pPlayer:GetNWBool( "bInRadio", false ) then
+        surface.PlaySound( "npc/metropolice/vo/off4.wav" )
+        return
+    end
+    -- if pPlayer == LocalPlayer() then return true end
+
+    -- return false
 end
 
 -- Can't do Schema:PopulateHelpMenu() because it's run before tTabs["plugins"] is added
