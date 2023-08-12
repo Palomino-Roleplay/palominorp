96a26bdab954725ba4070d4c51ef0e40d59b132d
sil <git@sil.dev>
2023-08-12 00:00:13 -0500

    Shrunk nameplates

--- plugins/prp_ui/libs/cl_nameplates.lua
+++ plugins/prp_ui/libs/cl_nameplates.lua
@@ -6,14 +6,14 @@ PRP.UI.Nameplates = PRP.UI.Nameplates or {}
 function PLUGIN:InitializedPlugins()
     surface.CreateFont( "PRP.UI.Nameplates.Name", {
         font = "Inter",
-        size = 32 * PRP.UI.ScaleFactor,
+        size = 26 * PRP.UI.ScaleFactor,
         weight = 700,
         antialias = true,
     } )
     
     surface.CreateFont( "PRP.UI.Nameplates.Name.Blurred", {
         font = "Inter",
-        size = 32 * PRP.UI.ScaleFactor,
+        size = 26 * PRP.UI.ScaleFactor,
         blursize = 4,
         weight = 700,
         antialias = true,
@@ -21,14 +21,14 @@ function PLUGIN:InitializedPlugins()
     
     surface.CreateFont( "PRP.UI.Nameplates.ID", {
         font = "Oxygen Mono",
-        size = 24 * PRP.UI.ScaleFactor,
+        size = 18 * PRP.UI.ScaleFactor,
         antialias = true,
     } )
     
     surface.CreateFont( "PRP.UI.Nameplates.Tag", {
         font = "Oxygen",
         weight = 700,
-        size = 20 * PRP.UI.ScaleFactor,
+        size = 18 * PRP.UI.ScaleFactor,
         antialias = true,
         additive = true,
         -- shadow = true,
@@ -37,7 +37,7 @@ function PLUGIN:InitializedPlugins()
     surface.CreateFont( "PRP.UI.Nameplates.Tag.Blurred", {
         font = "Oxygen",
         weight = 700,
-        size = 20 * PRP.UI.ScaleFactor,
+        size = 18 * PRP.UI.ScaleFactor,
         blursize = 3,
         antialias = true,
     } )
@@ -54,8 +54,8 @@ function PRP.UI.Nameplates.Draw( pPlayer )
     
     local iAlpha = math.Clamp( ( (iDistanceMultiplier * 0.25) + (0.75 * iVoiceMultiplier) ) * 255, 0, 255 )
     
-    -- draw.SimpleText( "NEW PLAYER", "PRP.UI.Nameplates.Tag", vPos:ToScreen().x, vPos:ToScreen().y - (20 + 4 + 32 * PRP.UI.ScaleFactor), Color( 255, 255, 255, iAlpha * 1 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
-    -- draw.SimpleText( "NEW PLAYER", "PRP.UI.Nameplates.Tag.Blurred", vPos:ToScreen().x, vPos:ToScreen().y - (20 + 4 + 32 * PRP.UI.ScaleFactor), Color( 0, 255, 0, iAlpha * 1 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
+    -- draw.SimpleText( "NEW PLAYER", "PRP.UI.Nameplates.Tag", vPos:ToScreen().x, vPos:ToScreen().y - (20 + 4 + 26 * PRP.UI.ScaleFactor), Color( 255, 255, 255, iAlpha * 1 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
+    -- draw.SimpleText( "NEW PLAYER", "PRP.UI.Nameplates.Tag.Blurred", vPos:ToScreen().x, vPos:ToScreen().y - (20 + 4 + 26 * PRP.UI.ScaleFactor), Color( 0, 255, 0, iAlpha * 1 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
 
     -- Equipping
     -- draw.SimpleText( pPlayer:Nick(), "PRP.UI.Nameplates.Name.Blurred", vPos:ToScreen().x, vPos:ToScreen().y - ((20 + 4) * PRP.UI.ScaleFactor), Color( 255, 255, 255, iAlpha ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
@@ -69,9 +69,9 @@ function PRP.UI.Nameplates.Draw( pPlayer )
     draw.SimpleText( "#" .. string.format( "%06d", pPlayer:GetCharacter():GetID() ), "PRP.UI.Nameplates.ID", vPos:ToScreen().x, vPos:ToScreen().y, Color( 255, 255, 255, iAlpha * 0.45 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
     
     -- RP Name
+    iYPos = iYPos - ((18 + 4) * PRP.UI.ScaleFactor)
     if pPlayer:GetNetVar( "actionString", nil ) then
         -- Name (Blurred)
-        iYPos = iYPos - ((20 + 4) * PRP.UI.ScaleFactor)
         draw.SimpleText( pPlayer:Nick(), "PRP.UI.Nameplates.Name.Blurred", vPos:ToScreen().x, iYPos, Color( 255, 255, 255, iAlpha ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
 
         -- Action
@@ -80,7 +80,6 @@ function PRP.UI.Nameplates.Draw( pPlayer )
         draw.SimpleText( string.upper( pPlayer:GetNetVar( "actionString", "" ) ), "PRP.UI.Nameplates.Tag", vPos:ToScreen().x, iYPos, Color( 255, 206, 73, iDistanceMultiplier * 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
     
     else    
-        iYPos = iYPos - ((20 + 4) * PRP.UI.ScaleFactor)
         draw.SimpleText( pPlayer:Nick(), "PRP.UI.Nameplates.Name", vPos:ToScreen().x, iYPos, Color( 137 + ( 118 * pPlayer:VoiceVolume() ), 191 + ( 64 * pPlayer:VoiceVolume() ), 255, iAlpha ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
     end
 
@@ -88,9 +87,9 @@ function PRP.UI.Nameplates.Draw( pPlayer )
     if GetConVar( "developer" ):GetBool() and LocalPlayer():IsAdmin() then
         local tPlyFaction = ix.faction.Get(pPlayer:Team())
 
-        iYPos = iYPos - (32 * PRP.UI.ScaleFactor)
+        iYPos = iYPos - (26 * PRP.UI.ScaleFactor)
         draw.SimpleText( tPlyFaction.name .. ( pPlayer:GetCharacter():GetClass() and (" (" .. (ix.class.Get(pPlayer:GetCharacter():GetClass()).name) .. ")") or ""), "DebugFixed", vPos:ToScreen().x, iYPos, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
-        
+
         iYPos = iYPos - (12 * PRP.UI.ScaleFactor)
         draw.SimpleText( "Distance: " .. math.Round( LocalPlayer():GetPos():Distance( pPlayer:GetPos() ), 2 ), "DebugFixed", vPos:ToScreen().x, iYPos, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM )
 
