11e82e1dd6451afecae88f361f4dc7f6cd3b1758
sil <git@sil.dev>
2023-08-12 23:17:04 -0500

    Temporary resolution fix

--- schema/libs/sv_api.lua
+++ schema/libs/sv_api.lua
@@ -5,6 +5,17 @@ PRP.API = PRP.API or {}
 PRP.UI = PRP.UI or {}
 PRP.UI.AllowedResolutions = PRP.UI.AllowedResolutions or {}
 
+PRP.UI.AllowedResolutions = {
+    ["1280x720"] = true,
+    ["1600x900"] = true,
+    ["1920x1080"] = true,
+    ["2560x1440"] = true,
+    ["3840x2160"] = true,
+    ["2560x1080"] = true,
+    ["3440x1440"] = true,
+    ["5120x2160"] = true
+}
+
 -- @TODO Hell no
 function PRP.API.Initialize()
     http.Fetch( PRP.API_URL .. "/ui/resolutions", function( sBody, _, _, iResponseCode )
@@ -35,4 +46,4 @@ function PRP.API.Initialize()
     end )
 end
 -- hook.Add( "InitPostEntity", "PRP.API.InitializedPlugins", PRP.API.Initialize )
-timer.Simple( 0, function() PRP.API.Initialize() end )
+-- timer.Simple( 0, function() PRP.API.Initialize() end )
