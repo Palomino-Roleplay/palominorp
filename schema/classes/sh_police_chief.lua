f7c76462b7642d393e9a549e011cfa6a5dc0cd5f
sil <git@sil.dev>
2023-08-15 15:11:40 -0500

    Added HK & P226 to Lieutenant & Chief

--- schema/classes/sh_police_chief.lua
+++ schema/classes/sh_police_chief.lua
@@ -3,6 +3,6 @@ CLASS.faction = FACTION_POLICE
 CLASS.isDefault = false
 CLASS.classLevel = 100
 CLASS.bodygroups = "012000500"
-CLASS.weapons = {"prp_batteringram", "prp_ticketbook"}
+CLASS.weapons = {"prp_batteringram", "prp_ticketbook", "cw_kk_hk416", "khr_p226"}
 
 CLASS_POLICE_CHIEF = CLASS.index
--- schema/classes/sh_police_lieutenant.lua
+++ schema/classes/sh_police_lieutenant.lua
@@ -3,6 +3,6 @@ CLASS.faction = FACTION_POLICE
 CLASS.isDefault = false
 CLASS.classLevel = 30
 CLASS.bodygroups = "023000300"
-CLASS.weapons = {"prp_batteringram", "prp_ticketbook"}
+CLASS.weapons = {"prp_batteringram", "prp_ticketbook", "cw_kk_hk416", "khr_p226"}
 
 CLASS_POLICE_LIEUTENANT = CLASS.index
