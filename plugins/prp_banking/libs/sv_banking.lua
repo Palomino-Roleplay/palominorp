21351786a3f51719a83c9142fa3f194b769e70d7
sil <git@sil.dev>
2023-08-15 19:11:41 -0500

    Removed debug prints

--- plugins/prp_banking/libs/sv_banking.lua
+++ plugins/prp_banking/libs/sv_banking.lua
@@ -4,8 +4,6 @@ PRP.Banking = PRP.Banking or {}
 function PRP.Banking.Create( cCharacter, fnCallback )
     ix.inventory.New( 0, "banking_character", function( oInventory )
         if ( oInventory ) then
-            Print( oInventory:GetID() )
-
             if fnCallback then
                 fnCallback( oInventory )
             end
@@ -20,15 +18,13 @@ function PRP.Banking.Get( cCharacter, fnCallback )
     local nInventoryID = cCharacter:GetData( "banking_inventory_id" )
 
     if ( nInventoryID ) then
-        Print("testo")
         return ix.item.inventories[ nInventoryID ]
     end
 end
 
+-- Access a character's banking inventory
 function PRP.Banking.Open( cCharacter )
     local oInventory = PRP.Banking.Get( cCharacter )
-
-    Print( oInventory )
 
     if not oInventory then return end
 
