fdb2b23974ab084074668785b89a736c62bf9a0c
sil <git@sil.dev>
2023-08-22 22:17:08 -0500

    Removed unused include

--- plugins/prp_jobutils/sh_plugin.lua
+++ plugins/prp_jobutils/sh_plugin.lua
@@ -4,6 +4,4 @@ PLUGIN.name = "Job Utilities"
 PLUGIN.author = "sil"
 PLUGIN.description = "Adds a job NPC and job car spawner."
 
-ix.util.Include("meta/sv_character.lua")
-
 ix.util.Include("hooks/sv_hooks.lua")
