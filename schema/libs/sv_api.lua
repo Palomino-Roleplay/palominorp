e594a71a1b1a7c1248519a0f8bdf589a8fd28901
sil <git@sil.dev>
2023-08-22 22:18:31 -0500

    WebSocket & REST API connection, authentication, and other functions

--- schema/libs/sv_api.lua
+++ schema/libs/sv_api.lua
@@ -1,5 +1,16 @@
 PRP = PRP or {}
+
+require( "gwsockets" )
+
 PRP.API = PRP.API or {}
+PRP.API.bInitialized = PRP.API.bInitialized or false
+
+PRP.API.SessionToken = PRP.API.SessionToken or nil
+PRP.API.ServerInfo = PRP.API.ServerInfo or {}
+
+PRP.API.Config = {
+    heartbeatInterval = 10
+}
 
 -- @TODO: AIDS
 PRP.UI = PRP.UI or {}
@@ -16,34 +27,212 @@ PRP.UI.AllowedResolutions = {
     ["5120x2160"] = true
 }
 
+-- @TODO: Change to local variable
+PRP._WebSocket = PRP._WebSocket or nil
+
+PRP.API.WS = PRP.API.WS or {}
+PRP.API.WS._tCallbacks = PRP.API.WS._tCallbacks or {}
+
+function PRP.API.WS.Log( sMessage )
+    MsgC(
+        Color( 255, 255, 255, 255 ),
+        "[",
+        Color( 137, 191, 255, 255 ),
+        "PAPI-WS",
+        Color( 255, 255, 255, 255 ),
+        "] ",
+        Color( 255, 255, 255, 255 ),
+        sMessage,
+        "\n"
+    )
+end
+
+PRP.API.REST = {}
+
+function PRP.API.REST.Log( sMessage )
+    MsgC(
+        Color( 255, 255, 255, 255 ),
+        "[",
+        Color( 163, 137, 255),
+        "PAPI-REST",
+        Color( 255, 255, 255, 255 ),
+        "] ",
+        Color( 255, 255, 255, 255 ),
+        sMessage,
+        "\n"
+    )
+end
+
+if pcall(require, "chttp") and CHTTP ~= nil then
+    PRP.HTTP = CHTTP
+    PRP.API.REST.Log( "Using CHTTP" )
+else
+    PRP.HTTP = HTTP
+    PRP.API.REST.Log( "Using HTTP" )
+end
+
+function PRP.API.WS.OnMessage( sType, fnCallback )
+    if not PRP.API.WS._tCallbacks[ sType ] then
+        PRP.API.WS._tCallbacks[ sType ] = {}
+    end
+
+    table.insert( PRP.API.WS._tCallbacks[ sType ], fnCallback )
+end
+
+function PRP.API.WS.Send( sType, tData )
+    if not PRP._WebSocket then return end
+
+    local tMessage = {
+        type = sType,
+        data = tData
+    }
+
+    PRP._WebSocket:write( util.TableToJSON( tMessage ) )
+end
+
+-- Makes sure REST API is working too
+local function fnInitializeREST()
+    PRP.API.REST.Log( "Attempting to connect to REST API server..." )
+
+    PRP.HTTP({
+        url = PRP.API_REST_URL,
+        method = "GET",
+        success = function( iResponseCode, sBody, tHeaders )
+            if iResponseCode == 200 then
+                local tBody = util.JSONToTable( sBody )
+
+                if not tBody then
+                    PRP.API.REST.Log( "Failed to parse body" )
+                    PRP.API.REST.Log( "Shutting down server..." )
+                    -- while true do end
+                end
+
+                PRP.API.REST.Log( "Successfully connected to PAPI (v" .. tBody.version .. ")" )
+
+                PRP.API.bInitialized = true
+                hook.Run( "PRP.API.Initialized", tBody.version )
+            else
+                PRP.API.REST.Log( "Failed to connect to REST API server: " .. iResponseCode )
+                PRP.API.REST.Log( "Shutting down server..." )
+                -- while true do end
+            end
+        end,
+        failed = function( err )
+            PRP.API.REST.Log( "Failed to connect to REST API server: " .. err )
+            PRP.API.REST.Log( "Shutting down server..." )
+            -- while true do end
+        end
+    })
+end
+
 -- @TODO Hell no
 function PRP.API.Initialize()
-    http.Fetch( PRP.API_URL .. "/ui/resolutions", function( sBody, _, _, iResponseCode )
-        if iResponseCode == 200 then
-            Print( "Successfully connected to API server" )
+    if PRP._WebSocket and PRP._WebSocket:isConnected() then
+        PRP.API.WS.Log( "Closing existing socket" )
+        PRP._WebSocket:close()
+    end
 
-            local tResolutions = util.JSONToTable( sBody )
-            if not tResolutions then
-                Print( "Failed to parse resolutions" )
-                Print( "Shutting down server..." )
-                while true do end
-            end
-
-            for _, tResolution in pairs( tResolutions ) do
-                PRP.UI.AllowedResolutions[ tResolution.width .. "x" .. tResolution.height ] = true
-            end
-
-            Print( "Loaded " .. table.Count( PRP.UI.AllowedResolutions ) .. " resolutions" )
-        else
-            Print( "Failed to connect to API server: " .. iResponseCode )
-            Print( "Shutting down server..." )
-            while true do end
-        end
-    end, function( err )
-        Print( "Failed to fetch resolutions: " .. err )
+    local sPalominoEnvironment = file.Read( "gamemodes/" .. Schema.folder .. "/palomino.json", true )
+    if not sPalominoEnvironment then
+        ErrorNoHalt( "CRITICAL: Failed to read palomino.json", "\n" )
         Print( "Shutting down server..." )
-        while true do end
-    end )
+
+        -- while true do end
+    end
+
+    local tPalominoEnvironment = util.JSONToTable( sPalominoEnvironment )
+    if not tPalominoEnvironment then
+        ErrorNoHalt( "CRITICAL: Failed to parse palomino.json", "\n" )
+        Print( "Shutting down server..." )
+
+        -- while true do end
+    end
+
+    PRP.API_REST_URL = tPalominoEnvironment.api.rest_url
+    PRP.API_WS_URL = tPalominoEnvironment.api.websocket_url
+
+    if not PRP.API_REST_URL then
+        ErrorNoHalt( "CRITICAL ERROR: Missing API REST URL", "\n" )
+        Print( "Shutting down server..." )
+
+        -- while true do end
+    end
+
+    if not PRP.API_WS_URL then
+        ErrorNoHalt( "CRITICAL ERROR: Missing API WS URL", "\n" )
+        Print( "Shutting down server..." )
+
+        -- while true do end
+    end
+
+    PRP._WebSocket = GWSockets.createWebSocket( PRP.API_WS_URL )
+    PRP._WebSocket:setHeader( "x-api-key", tPalominoEnvironment.api.key )
+
+    function PRP._WebSocket:onMessage( sMessage )
+        -- PRP.API.WS.Log( "Received: ", sMessage )
+
+        -- Convert message from JSON to table
+        local tMessage = util.JSONToTable( sMessage )
+
+        -- Check if the message is valid
+        if not tMessage then
+            ErrorNoHalt( "Invalid PAPI-WS message received: " .. sMessage, "\n" )
+            return
+        end
+
+        if tMessage.type == "auth" then
+            PRP.API.SessionToken = tMessage.sessionToken
+            PRP.API.ServerInfo = tMessage.serverInfo
+
+            PRP.API.WS.Log( "Successfully authenticated server as \"" .. PRP.API.ServerInfo.name .. "\"" )
+            Print( "Session token: ", PRP.API.SessionToken )
+
+            fnInitializeREST()
+        end
+
+        if not PRP.API.bInitialized then
+            PRP.API.WS.Log( "Server not initialized. Ignoring message of type \"" .. tMessage.type .. "\"" )
+            return
+        end
+
+        -- Run all callbacks for this message type
+        if PRP.API.WS._tCallbacks[ tMessage.type ] then
+            for _, fnCallback in pairs( PRP.API.WS._tCallbacks[ tMessage.type ] ) do
+                fnCallback( tMessage.data )
+            end
+        end
+    end
+
+    function PRP._WebSocket:onError( sErrorMessage )
+        ErrorNoHalt("Error: ", sErrorMessage, "\n" )
+    end
+
+    function PRP._WebSocket:onConnected()
+        PRP.API.WS.Log( "Connected to WebSocket server." )
+
+        timer.Create("PRP.API.WS.Heartbeat", 10, 0, function()
+            if not PRP.API.bInitialized then return end
+            PRP._WebSocket:write( util.TableToJSON( { type = "heartbeat" } ) )
+        end)
+    end
+
+    function PRP._WebSocket:onDisconnected()
+        PRP.API.WS.Log( "Disconnected from WebSocket server." )
+
+        timer.Remove("PRP.API.WS.Heartbeat")
+
+        -- @TODO: Attempt reconnect w/ session token
+    end
+
+    PRP.API.WS.Log( "Attempting to connect to WebSocket server..." )
+    PRP._WebSocket:open()
 end
--- hook.Add( "InitPostEntity", "PRP.API.InitializedPlugins", PRP.API.Initialize )
+hook.Add( "InitPostEntity", "PRP.API.InitPostEntity", PRP.API.Initialize )
 -- timer.Simple( 0, function() PRP.API.Initialize() end )
+
+-- @TODO: Remove
+PRP.API.Initialize()
+
+concommand.Add( "prp_api_init", function()
+    PRP.API.Initialize()
+end )
