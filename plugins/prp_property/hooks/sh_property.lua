28bc786004c57e4f764de5a69d394bba6597d342
sil <git@sil.dev>
2023-08-27 04:19:33 -0500

    We're not going to be using CanPlayerAccessDoor either.

--- plugins/prp_property/hooks/sh_property.lua
+++ plugins/prp_property/hooks/sh_property.lua
@@ -1,14 +1,8 @@
 local PLUGIN = PLUGIN
 
 function PLUGIN:InitPostEntity()
+    -- @TODO: We should probably do this in a different hook.
     for sPropertyID, oProperty in pairs( PRP.Property.GetAll() ) do
         oProperty:Init()
-    end
-end
-
--- @TODO: We probably want to make our own way of checking door access & rip out helix's entirely
-function PLUGIN:CanPlayerAccessDoor( pPlayer, eEntity, iAccessType )
-    if eEntity:GetProperty() then
-        return eEntity:GetProperty():HasAccess( pPlayer )
     end
 end
