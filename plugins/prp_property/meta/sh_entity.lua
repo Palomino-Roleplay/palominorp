ab9e1e11c5f66662821ad4cbf90eef88917187f5
sil <git@sil.dev>
2023-08-27 04:20:26 -0500

    Better entity meta property stuffs

--- plugins/prp_property/meta/sh_entity.lua
+++ plugins/prp_property/meta/sh_entity.lua
@@ -1,16 +1,47 @@
 local ENTITY = FindMetaTable( "Entity" )
 
-AccessorFunc( ENTITY, "m_oProperty", "Property" )
+AccessorFunc( ENTITY, "m_pSpawner", "Spawner" )
+
 function ENTITY:GetProperty()
     if self.m_oProperty then return self.m_oProperty end
 
-    local sPropertyID = self:GetNW2String( "PRP.Prop.Property", nil )
+    local sPropertyID = self:GetNWString( "PRP.Property", nil )
     if sPropertyID then
         self.m_oProperty = PRP.Property.Get( sPropertyID )
         return self.m_oProperty
     end
 
-    return nil
+    return
 end
 
-AccessorFunc( ENTITY, "m_pSpawner", "Spawner" )
+function ENTITY:SetProperty( oProperty )
+    if not oProperty then return end
+
+    -- @TODO I'm sure we can do this better with our own networking
+    self:SetNWString( "PRP.Property", oProperty:GetID() )
+end
+
+function ENTITY:HasAccess( cCharacter )
+    if not cCharacter then return false end
+    if not self:GetProperty() then return false end
+
+    return self:GetProperty():HasAccess( cCharacter )
+end
+
+
+
+function ENTITY:SetCategory( tCategory )
+
+end
+
+function ENTITY:GetCategory()
+
+end
+
+function ENTITY:GetCategoryID()
+
+end
+
+function ENTITY:GetRootCategory()
+
+end
