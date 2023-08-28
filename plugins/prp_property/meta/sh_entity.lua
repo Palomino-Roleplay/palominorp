4ca88760a1f752132a9b9b138f7a756cddbf9158
sil <git@sil.dev>
2023-08-27 19:24:57 -0500

    Added Entity GetCategoryID, SetCategory, GetCategory

--- plugins/prp_property/meta/sh_entity.lua
+++ plugins/prp_property/meta/sh_entity.lua
@@ -17,6 +17,8 @@ end
 function ENTITY:SetProperty( oProperty )
     if not oProperty then return end
 
+    if CLIENT then return end
+
     -- @TODO I'm sure we can do this better with our own networking
     self:SetNWString( "PRP.Property", oProperty:GetID() )
 end
@@ -29,3 +31,33 @@ function ENTITY:HasAccess( cCharacter )
 end
 
 AccessorFunc( ENTITY, "m_oCategory", "Category" )
+
+function ENTITY:GetCategoryID()
+    if self.m_sCategoryID then return self.m_sCategoryID end
+    if not self.m_oCategory then return end
+
+    self.m_sCategoryID = self.m_oCategory:GetID()
+    return self.m_sCategoryID
+end
+
+function ENTITY:SetCategory( oCategory )
+    self.m_oCategory = oCategory
+    self.m_sCategoryID = oCategory:GetID()
+
+    if CLIENT then return end
+
+    -- @TODO: Do our own networking (NWString has a limit)
+    self:SetNWString( "PRP.Category", self.m_sCategoryID )
+end
+
+function ENTITY:GetCategory()
+    if self.m_oCategory then return self.m_oCategory end
+
+    local sCategoryID = self:GetNWString( "PRP.Category", nil )
+    if sCategoryID then
+        self.m_oCategory = PRP.Prop.Category.Get( sCategoryID )
+        return self.m_oCategory
+    end
+
+    return nil
+end
