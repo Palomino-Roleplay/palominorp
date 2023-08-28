ef0bff691f2b74ed3852c58ea573703e0f179db7
sil <git@sil.dev>
2023-08-28 02:32:28 -0500

    Display blacklist zones, collisions

--- plugins/prp_property/hooks/cl_physgun.lua
+++ plugins/prp_property/hooks/cl_physgun.lua
@@ -26,23 +26,21 @@ function PLUGIN:PostDrawTranslucentRenderables()
 
     local iFloorZ = oProperty:GetFloorZ()
 
-    local vHitBoxMin, vHitBoxMax = PRP.Prop.PhysgunnedEntity:GetCollisionBounds()
+    local vHitBoxMin, vHitBoxMax = PRP.Prop.PhysgunnedEntity:OBBMins(), PRP.Prop.PhysgunnedEntity:OBBMaxs()
     local vHitBoxMinWorld = PRP.Prop.PhysgunnedEntity:LocalToWorld( vHitBoxMin )
     local vHitBoxMaxWorld = PRP.Prop.PhysgunnedEntity:LocalToWorld( vHitBoxMax )
 
-    if bIsDefensiveProp then
+    if string.StartsWith( oCategory:GetID(), "defensive_props" ) then
         vTargetPos.z = iFloorZ
         vHitBoxMinWorld.z = iFloorZ
     end
-
-    local bIntersectingAny = false
 
     local tSnappedPoints, tUnsnappedPoints = PRP.Prop.PhysgunnedEntity:CalcSnappingPoints( true )
 
     render.SetColorMaterial()
 
     -- Unsnapped Points
-    for _, tUnsnappedPoint in pairs( tUnsnappedPoints ) do
+    for _, tUnsnappedPoint in pairs( tUnsnappedPoints or {} ) do
         if tUnsnappedPoint.worldPoint:DistToSqr( LocalPlayer():GetPos() ) > 262144 then continue end
 
         render.DrawSphere(
@@ -160,111 +158,152 @@ function PLUGIN:PostDrawTranslucentRenderables()
     -- end
     -- We have a pair of eligible snap points!
 
-    if true then return end
+    -- if #tDisplayedSnapPoints > 0 then
+    --     local tSnapPoint1 = tDisplayedSnapPoints[ 1 ]
+    --     local tSnapPoint2 = tDisplayedSnapPoints[ 2 ]
 
-    if #tDisplayedSnapPoints > 0 then
-        local tSnapPoint1 = tDisplayedSnapPoints[ 1 ]
-        local tSnapPoint2 = tDisplayedSnapPoints[ 2 ]
+    --     -- Initial angles and grid
+    --     local targetAng = tSnapPoint1.ent:GetAngles()
+    --     local angleGrid = tSnapPoint1.snap.angleGrid
 
-        -- Initial angles and grid
-        local targetAng = tSnapPoint1.ent:GetAngles()
-        local angleGrid = tSnapPoint1.snap.angleGrid
+    --     Print( angleGrid )
 
-        Print( angleGrid )
+    --     -- Local angle difference
+    --     local angleDiff = vTargetAng - targetAng
+    --     angleDiff:Normalize()
 
-        -- Local angle difference
-        local angleDiff = vTargetAng - targetAng
-        angleDiff:Normalize()
+    --     -- @TODO: This doesn't actually work for rotation in more than one axis
+    --     local function SnapToGrid(angle, grid)
+    --         return (grid == 360) and 0 or math.Round(angle / grid) * grid
+    --     end
 
-        -- @TODO: This doesn't actually work for rotation in more than one axis
-        local function SnapToGrid(angle, grid)
-            return (grid == 360) and 0 or math.Round(angle / grid) * grid
-        end
+    --     -- Snap angles
+    --     angleDiff:SetUnpacked(SnapToGrid(angleDiff.p, angleGrid.p), SnapToGrid(angleDiff.y, angleGrid.y), SnapToGrid(angleDiff.r, angleGrid.r))
 
-        -- Snap angles
-        angleDiff:SetUnpacked(SnapToGrid(angleDiff.p, angleGrid.p), SnapToGrid(angleDiff.y, angleGrid.y), SnapToGrid(angleDiff.r, angleGrid.r))
+    --     -- Calculate final snapped angles without altering the PhysgunnedEntity
+    --     local finalAng = targetAng + angleDiff
+    --     finalAng:Normalize()
 
-        -- Calculate final snapped angles without altering the PhysgunnedEntity
-        local finalAng = targetAng + angleDiff
-        finalAng:Normalize()
+    --     -- Manually calculate world point for the snapped angle
+    --     local mat = Matrix()
+    --     mat:SetAngles(finalAng)
+    --     local vSnapPoint2Local = tSnapPoint2.snap.point
+    --     local vSnapPoint2WorldAdjusted = mat * vSnapPoint2Local + PRP.Prop.PhysgunnedEntity:GetPos()
 
-        -- Manually calculate world point for the snapped angle
-        local mat = Matrix()
-        mat:SetAngles(finalAng)
-        local vSnapPoint2Local = tSnapPoint2.snap.point
-        local vSnapPoint2WorldAdjusted = mat * vSnapPoint2Local + PRP.Prop.PhysgunnedEntity:GetPos()
+    --     -- Draw connecting line
+    --     local vSnapPoint1World = tSnapPoint1.ent:LocalToWorld(tSnapPoint1.snap.point)
+    --     render.DrawLine(vSnapPoint1World, tSnapPoint2.ent:LocalToWorld( tSnapPoint2.snap.point ), oSnapPointDisplayColor, false)
 
-        -- Draw connecting line
-        local vSnapPoint1World = tSnapPoint1.ent:LocalToWorld(tSnapPoint1.snap.point)
-        render.DrawLine(vSnapPoint1World, tSnapPoint2.ent:LocalToWorld( tSnapPoint2.snap.point ), oSnapPointDisplayColor, false)
+    --     -- Calculate position offset
+    --     local vAlignVector = vSnapPoint1World - vSnapPoint2WorldAdjusted
+    --     vTargetPos = PRP.Prop.PhysgunnedEntity:GetPos() + vAlignVector
 
-        -- Calculate position offset
-        local vAlignVector = vSnapPoint1World - vSnapPoint2WorldAdjusted
-        vTargetPos = PRP.Prop.PhysgunnedEntity:GetPos() + vAlignVector
+    --     vTargetAng = finalAng
+    -- end
 
-        vTargetAng = finalAng
-    end
+    -- ---------------------
+    -- -- /SNAP POINTS :) --
+    -- ---------------------
 
-    ---------------------
-    -- /SNAP POINTS :) --
-    ---------------------
+    -- Print( PRP.Prop.PhysgunnedEntity:IsInZoneOfType( "prop_blacklist" ) )
 
-    -- prop_blacklist zone
-    local tBlacklistZones = oProperty:GetZonesOfType( "prop_blacklist" )
-    for _, oZone in pairs( tBlacklistZones ) do
-        local vZoneMidpoint = ( oZone.pos[ 1 ] + oZone.pos[ 2 ] ) / 2
-
-        local vZoneSize = Vector(
-            math.abs(oZone.pos[1].x - oZone.pos[2].x) / 2,
-            math.abs(oZone.pos[1].y - oZone.pos[2].y) / 2,
-            math.abs(oZone.pos[1].z - oZone.pos[2].z) / 2
-        )
-
-        local flZoneMaxAxis = math.max( vZoneSize.x, vZoneSize.y )
-        local flZoneMinAxis = math.min( vZoneSize.x, vZoneSize.y )
-
-        local flDistance = vTargetPos:DistToSqr( vZoneMidpoint )
-        local flShapeRatio = flZoneMaxAxis / flZoneMinAxis
-        local flDistanceFraction = ( ( flDistance / ( flZoneMaxAxis * flZoneMaxAxis ) ) / flShapeRatio ) / 2
-
-        -- local flFadePercentage = math.Clamp( flDistanceFraction / flShapeRatio, 0, 1 )
-
-        local flDisplayFaction = math.min( 1 - flDistanceFraction, 1 )
-
-        if flDisplayFaction < 0 then continue end
-
-        local bIntersectingZone = util.IsOBBIntersectingOBB(
-            vTargetPos,
-            vTargetAng,
-            vHitBoxMin,
-            vHitBoxMax,
-            vZoneMidpoint,
-            Angle(0, 0, 0),
-            -vZoneSize,
-            vZoneSize,
-            0
-        )
+    -- Draw property blacklist zones
+    local tBlacklistZones = oProperty:GetZonesOfType("prop_blacklist")
+    for _, tZone in pairs(tBlacklistZones) do
         render.DrawWireframeBox(
-            oZone.pos[ 1 ],
-            Angle( 0, 0, 0 ),
-            Vector( 0, 0, 0 ),
-            oZone.pos[ 2 ] - oZone.pos[ 1 ],
-            Color( 0, 0, 0, 255 * flDisplayFaction )
-        )
-
-        render.SetColorMaterial()
-
-        render.DrawBox(
-            oZone.pos[ 1 ],
-            Angle( 0, 0, 0 ),
-            Vector( 0, 0, 0 ),
-            oZone.pos[ 2 ] - oZone.pos[ 1 ],
-            Color( 255, 0, 0, 32 * flDisplayFaction ),
+            tZone.pos[1],
+            Angle(0, 0, 0),
+            Vector(0, 0, 0),
+            tZone.pos[2] - tZone.pos[1],
+            Color(255, 0, 0),
             true
         )
-
-        bIntersectingAny = bIntersectingAny or bIntersectingZone
     end
+
+    -- prop_blacklist zone
+    -- local tBlacklistZones = oProperty:GetZonesOfType( "prop_blacklist" )
+    -- for _, oZone in pairs( tBlacklistZones ) do
+    --     local vZoneMidpoint = ( oZone.pos[ 1 ] + oZone.pos[ 2 ] ) / 2
+
+    --     local vZoneSize = Vector(
+    --         math.abs(oZone.pos[1].x - oZone.pos[2].x) / 2,
+    --         math.abs(oZone.pos[1].y - oZone.pos[2].y) / 2,
+    --         math.abs(oZone.pos[1].z - oZone.pos[2].z) / 2
+    --     )
+
+    --     local flZoneMaxAxis = math.max( vZoneSize.x, vZoneSize.y )
+    --     local flZoneMinAxis = math.min( vZoneSize.x, vZoneSize.y )
+
+    --     local flDistance = vTargetPos:DistToSqr( vZoneMidpoint )
+    --     local flShapeRatio = flZoneMaxAxis / flZoneMinAxis
+    --     local flDistanceFraction = ( ( flDistance / ( flZoneMaxAxis * flZoneMaxAxis ) ) / flShapeRatio ) / 2
+
+    --     -- local flFadePercentage = math.Clamp( flDistanceFraction / flShapeRatio, 0, 1 )
+
+    --     local flDisplayFaction = math.min( 1 - flDistanceFraction, 1 )
+
+    --     if flDisplayFaction < 0 then continue end
+
+    --     local bIntersectingZone = util.IsOBBIntersectingOBB(
+    --         vTargetPos,
+    --         vTargetAng,
+    --         vHitBoxMin,
+    --         vHitBoxMax,
+    --         vZoneMidpoint,
+    --         Angle(0, 0, 0),
+    --         -vZoneSize,
+    --         vZoneSize,
+    --         0
+    --     )
+    --     render.DrawWireframeBox(
+    --         oZone.pos[ 1 ],
+    --         Angle( 0, 0, 0 ),
+    --         Vector( 0, 0, 0 ),
+    --         oZone.pos[ 2 ] - oZone.pos[ 1 ],
+    --         Color( 0, 0, 0, 255 * flDisplayFaction )
+    --     )
+
+    --     render.SetColorMaterial()
+
+    --     render.DrawBox(
+    --         oZone.pos[ 1 ],
+    --         Angle( 0, 0, 0 ),
+    --         Vector( 0, 0, 0 ),
+    --         oZone.pos[ 2 ] - oZone.pos[ 1 ],
+    --         Color( 255, 0, 0, 32 * flDisplayFaction ),
+    --         true
+    --     )
+
+    --     bIntersectingAny = bIntersectingAny or bIntersectingZone
+    -- end
+
+    local bIntersectingAny = false
+    local tIntersectingEntities = PRP.Prop.PhysgunnedEntity:CalcIntersect( vTargetPos, vTargetAng )
+
+    Print( "Intersecting:" )
+    Print( tIntersectingEntities )
+
+    render.DrawWireframeBox(
+        vTargetPos,
+        vTargetAng,
+        vHitBoxMin,
+        vHitBoxMax,
+        Color( 255, 255, 255 ),
+        false
+    )
+
+    for _, eEntity in pairs( tIntersectingEntities ) do
+        render.DrawWireframeBox(
+            eEntity:GetPos(),
+            eEntity:GetAngles(),
+            eEntity:OBBMins(),
+            eEntity:OBBMaxs(),
+            Color( 255, 0, 0 ),
+            false
+        )
+    end
+
+    if true then return end
 
     -- Collision w/ other entities
     for _, eEntity in pairs( ents.FindInBox( vHitBoxMinWorld, vHitBoxMaxWorld ) ) do
