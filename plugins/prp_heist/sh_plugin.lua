ec133ff3ffc74c9d060523716f487d80ab62c41b
sil <git@sil.dev>
2023-09-01 18:28:33 -0500

    New config option: terminalHackTime

--- plugins/prp_heist/sh_plugin.lua
+++ plugins/prp_heist/sh_plugin.lua
@@ -7,3 +7,8 @@ PLUGIN.description = ""
 PRP.Heist = PRP.Heist or {}
 
 ix.util.Include( "meta/sh_heist.lua" )
+
+ix.config.Add( "terminalHackTime", 30, "How long terminals take to hack.", nil, {
+    data = { min = 1, max = 120 },
+    category = "Palomino: Heist"
+} )
