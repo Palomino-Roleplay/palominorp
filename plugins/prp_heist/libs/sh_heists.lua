5097813db28aefb07cad5c2de3ed7e25cadc8b01
sil <git@sil.dev>
2023-09-01 21:42:47 -0500

    Heist plugin master list

---
+++ plugins/prp_heist/libs/sh_heists.lua
@@ -0,0 +1,22 @@
+local PLUGIN = PLUGIN
+
+PRP.Heist = PRP.Heist or {}
+PRP.Heist.List = PRP.Heist.List or {}
+
+function PRP.Heist.Register( oHeist )
+    PRP.Heist.List[oHeist:GetID()] = oHeist
+
+    oHeist:Init()
+end
+
+function PRP.Heist.Get( sHeist )
+    return PRP.Heist.List[sHeist]
+end
+
+function PRP.Heist.GetAll()
+    return PRP.Heist.List
+end
+
+function PRP.Heist.GetRentable()
+    return PRP.Heist.Rentable
+end
