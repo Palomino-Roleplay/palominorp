4afa8b24941b4990bf8ef25f38c2874c20f675b9
sil <git@sil.dev>
2023-09-03 22:40:00 -0500

    Soil deployable item

---
+++ plugins/prp_weed/items/deployable/sh_weed_soil.lua
@@ -0,0 +1,8 @@
+
+ITEM.name = "Soil"
+ITEM.model = "models/zerochain/props_growop2/zgo2_soil.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.description = "Soil for growing plants."
+ITEM.category = "Deployable"
+ITEM.entClass = "zgo2_soil"
