a9b6bcdeb9a160070e65a66e25c9491a3a7de728
sil <git@sil.dev>
2023-09-04 20:30:22 -0500

    Added jar

---
+++ plugins/prp_weed/items/deployable/sh_weed_jar.lua
@@ -0,0 +1,8 @@
+
+ITEM.name = "Jar"
+ITEM.model = "models/zerochain/props_growop2/zgo2_jar.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.description = "A jar to store totally legal stuff in."
+ITEM.category = "Weed"
+ITEM.entClass = "zgo2_jar"
