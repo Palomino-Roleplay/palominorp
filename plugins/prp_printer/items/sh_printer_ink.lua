7f9f1dabed415e8d55497a9fba78a15983cf59b7
sil <git@sil.dev>
2023-09-26 11:34:54 -0500

    Money printer stub

---
+++ plugins/prp_printer/items/sh_printer_ink.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Printer Ink"
+ITEM.model = "models/props_c17/TrapPropeller_Lever.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.description = "A cartridge of ink for a printer, almost as valuable as gold."
+ITEM.category = "Printer"
---
+++ plugins/prp_printer/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Palomino: Money Printer"
+PLUGIN.author = "sil"
+PLUGIN.description = ""
