e18e9b7fffd823a4f1c36b029915aed89e54c8d7
sil <git@sil.dev>
2023-10-06 23:37:38 -0500

    Weed hook SWEP done

---
+++ entities/weapons/prp_weed_hook.lua
@@ -0,0 +1,57 @@
+SWEP.PrintName              = "Drying Rack"
+SWEP.Author                 = "sil"
+SWEP.Instructions           = "Left click to find out"
+SWEP.Category               = "Palomino"
+
+SWEP.Spawnable              = true
+SWEP.AdminOnly              = true
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo		    = "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		    = "none"
+
+SWEP.ViewModel			    = ""
+SWEP.WorldModel			    = "models/zerochain/props_growop2/zgo2_dryline.mdl"
+SWEP.UseHands               = true
+
+SWEP.DrawAmmo               = false
+
+function SWEP:Initialize()
+    self:SetHoldType("revolver")
+end
+
+function SWEP:PrimaryAttack()
+    if CLIENT then return end
+
+    local pPlayer = self:GetOwner()
+    local tTrace = pPlayer:GetEyeTrace()
+
+    local vStart = tTrace.HitPos
+    local vNormal = tTrace.HitNormal
+
+    local eEntity = ents.Create( "zgo2_dryline" )
+    eEntity:SetPos( vStart )
+    eEntity:SetAngles( vNormal:Angle() + Angle( 90, 0, 0 ) )
+    eEntity:SetWallEndAngle( vNormal:Angle() + Angle( 90, 0, 0 ) )
+
+    eEntity:Spawn()
+    eEntity:Activate()
+
+    eEntity:SetWallAngle( vNormal:Angle() )
+
+    eEntity:EmitSound("weapons/crossbow/hit1.wav")
+
+    zclib.Player.SetOwner( eEntity, pPlayer )
+
+    self:GetOwner():StripWeapon( self:GetClass() )
+end
+
+function SWEP:SecondaryAttack()
+    return
+end
