3ae6c4518675b76c4e4dc87a75d74f9573362db8
sil <git@sil.dev>
2023-10-08 15:18:10 -0500

    Additional heist configs

--- plugins/prp_heist/sh_plugin.lua
+++ plugins/prp_heist/sh_plugin.lua
@@ -7,13 +7,29 @@ PLUGIN.description = ""
 PRP.Heist = PRP.Heist or {}
 
 ix.util.Include( "meta/sh_heist.lua" )
+ix.util.Include( "meta/sv_player.lua" )
+
+ix.config.Add( "heistLootTime", 10, "Number of seconds it takes to grab money.", nil, {
+    data = { min = 1, max = 30 },
+    category = "Palomino: Heists"
+} )
 
 ix.config.Add( "terminalHackTime", 30, "How long terminals take to hack.", nil, {
     data = { min = 1, max = 120 },
-    category = "Palomino: Heist"
+    category = "Palomino: Heists"
 } )
 
 ix.config.Add( "gasPropagateTime", 30, "How long it takes for the gas canister to propagate across the entire laser hallway.", nil, {
     data = { min = 1, max = 120 },
-    category = "Palomino: Heist"
+    category = "Palomino: Heists"
+} )
+
+ix.config.Add( "heistLootDistance", 15000000, "Square of the distance required for them to be considered safe.", nil, {
+    data = { min = 100000, max = 1000000000 },
+    category = "Palomino: Heists"
+} )
+
+ix.config.Add( "heistLootTimer", 15, "Number of minutes a player has to stay away from the bank to be get the loot.", nil, {
+    data = { min = 1, max = 30 },
+    category = "Palomino: Heists"
 } )
