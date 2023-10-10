9f204df463a47f57e175b63157356df3bb79fea9
sil <git@sil.dev>
2023-10-09 21:14:01 -0500

    Added black balaclava

---
+++ schema/items/pacoutfit/sh_black_balaclava.lua
@@ -0,0 +1,68 @@
+ITEM.name = "Black Balaclava"
+ITEM.description = "Hides your identity."
+ITEM.model = "models/sal/halloween/ninja.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.outfitCategory = "hat"
+ITEM.pacData = {
+    [1] = {
+        ["children"] = {
+            [1] = {
+                ["children"] = {
+                },
+                ["self"] = {
+                    ["Skin"] = 0,
+                    ["UniqueID"] = "b9d9d6cdb12f8690fa20c3867064616f44cca1ee52a1eae862d2a6b71c4fd813",
+                    ["NoLighting"] = false,
+                    ["AimPartName"] = "",
+                    ["IgnoreZ"] = false,
+                    ["AimPartUID"] = "",
+                    ["Materials"] = "",
+                    ["Name"] = "",
+                    ["LevelOfDetail"] = 0,
+                    ["NoTextureFiltering"] = false,
+                    ["PositionOffset"] = Vector(0, 0, 0),
+                    ["IsDisturbing"] = false,
+                    ["EyeAngles"] = false,
+                    ["DrawOrder"] = 0,
+                    ["TargetEntityUID"] = "",
+                    ["Alpha"] = 1,
+                    ["Material"] = "",
+                    ["Invert"] = false,
+                    ["ForceObjUrl"] = false,
+                    ["Bone"] = "eyes",
+                    ["Angles"] = Angle(1.7075475398087e-06, -1.2518906593323, 2.1344342258089e-06),
+                    ["AngleOffset"] = Angle(0, 0, 0),
+                    ["BoneMerge"] = false,
+                    ["Color"] = Vector(1, 1, 1),
+                    ["Position"] = Vector(-4.3817749023438, 0.3297119140625, -2.2934188842773),
+                    ["ClassName"] = "model2",
+                    ["Brightness"] = 1,
+                    ["Hide"] = false,
+                    ["NoCulling"] = false,
+                    ["Scale"] = Vector(1, 1, 1),
+                    ["LegacyTransform"] = false,
+                    ["EditorExpand"] = false,
+                    ["Size"] = 1,
+                    ["ModelModifiers"] = "",
+                    ["Translucent"] = false,
+                    ["BlendMode"] = "",
+                    ["EyeTargetUID"] = "",
+                    ["Model"] = "models/sal/halloween/ninja.mdl",
+                },
+            },
+        },
+        ["self"] = {
+            ["DrawOrder"] = 0,
+            ["UniqueID"] = "39fd85ba79aa1daca235416d6c41bebc7c7b7f8c48f5156f25f614bed4cab6f2",
+            ["Hide"] = false,
+            ["TargetEntityUID"] = "",
+            ["EditorExpand"] = true,
+            ["OwnerName"] = "self",
+            ["IsDisturbing"] = false,
+            ["Name"] = "my outfit",
+            ["Duplicate"] = false,
+            ["ClassName"] = "group",
+        },
+    },
+}
