b99a2ebbf3067604bba94726c48cb21ddb352765
sil <git@sil.dev>
2023-10-09 21:28:45 -0500

    Added flat cap

---
+++ schema/items/pacoutfit/sh_flatcap.lua
@@ -0,0 +1,68 @@
+ITEM.name = "Flat Cap"
+ITEM.description = "Peaky???"
+ITEM.model = "models/modified/hat06.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.outfitCategory = "hat"
+ITEM.pacData = {
+    [1] = {
+        ["children"] = {
+            [1] = {
+                ["children"] = {
+                },
+                ["self"] = {
+                    ["Skin"] = 0,
+                    ["UniqueID"] = "533fe10fad4feb0e89e8edce4da1e6dfdc02d334c4c37fa6634235bc67ca22ca",
+                    ["NoLighting"] = false,
+                    ["AimPartName"] = "",
+                    ["IgnoreZ"] = false,
+                    ["AimPartUID"] = "",
+                    ["Materials"] = "",
+                    ["Name"] = "",
+                    ["LevelOfDetail"] = 0,
+                    ["NoTextureFiltering"] = false,
+                    ["PositionOffset"] = Vector(0, 0, 0),
+                    ["IsDisturbing"] = false,
+                    ["EyeAngles"] = false,
+                    ["DrawOrder"] = 0,
+                    ["TargetEntityUID"] = "",
+                    ["Alpha"] = 1,
+                    ["Material"] = "",
+                    ["Invert"] = false,
+                    ["ForceObjUrl"] = false,
+                    ["Bone"] = "eyes",
+                    ["Angles"] = Angle(0, 0, 0),
+                    ["AngleOffset"] = Angle(0, 0, 0),
+                    ["BoneMerge"] = false,
+                    ["Color"] = Vector(1, 1, 1),
+                    ["Position"] = Vector(-3.5371704101563, -0.30169677734375, 2.3078384399414),
+                    ["ClassName"] = "model2",
+                    ["Brightness"] = 1,
+                    ["Hide"] = false,
+                    ["NoCulling"] = false,
+                    ["Scale"] = Vector(1, 1, 1),
+                    ["LegacyTransform"] = false,
+                    ["EditorExpand"] = false,
+                    ["Size"] = 0.94,
+                    ["ModelModifiers"] = "",
+                    ["Translucent"] = false,
+                    ["BlendMode"] = "",
+                    ["EyeTargetUID"] = "",
+                    ["Model"] = "models/modified/hat06.mdl",
+                },
+            },
+        },
+        ["self"] = {
+            ["DrawOrder"] = 0,
+            ["UniqueID"] = "39fd85ba79aa1daca235416d6c41bebc7c7b7f8c48f5156f25f614bed4cab6f2",
+            ["Hide"] = false,
+            ["TargetEntityUID"] = "",
+            ["EditorExpand"] = true,
+            ["OwnerName"] = "self",
+            ["IsDisturbing"] = false,
+            ["Name"] = "my outfit",
+            ["Duplicate"] = false,
+            ["ClassName"] = "group",
+        },
+    },
+}
