7549e0c3ec8f4a7ac219708ef92556e0c8a3c190
sil <git@sil.dev>
2023-10-11 21:10:35 -0500

    some vehicle dealer starter stuff

---
+++ plugins/prp_vehicle_dealer/config/sh_vehicles.lua
@@ -0,0 +1,13 @@
+PRP.VehicleDealer = PRP.VehicleDealer or {}
+
+PRP.VehicleDealer.Config = PRP.VehicleDealer.Config or {}
+
+PRP.VehicleDealer.Config.Vehicles = {
+    ["gmc_savana"] = {
+        class = "gmc_savana_lw",
+        make = "GMC",
+        model = "Savana",
+        price = 10000,
+        category = "Vans",
+    }
+}
