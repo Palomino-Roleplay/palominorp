edbd6bffb811c62331cae5a865f206fb9891182b
sil <git@sil.dev>
2023-10-12 17:07:50 -0500

    hide the player interaction menu for the playtest

--- plugins/prp_ui/libs/cl_ui.lua
+++ plugins/prp_ui/libs/cl_ui.lua
@@ -56,162 +56,162 @@ surface.CreateFont( "PRP.UI.Interaction.Option", {
     antialias = true
 })
 
-hook.Add( "PostDrawTranslucentRenderables", "PUI.DrawInteractionEffects", function()
-	render.SetStencilWriteMask( 0xFF )
-	render.SetStencilTestMask( 0xFF )
-	render.SetStencilReferenceValue( 0 )
-	render.SetStencilCompareFunction( STENCIL_ALWAYS )
-	render.SetStencilPassOperation( STENCIL_KEEP )
-	render.SetStencilFailOperation( STENCIL_KEEP )
-	render.SetStencilZFailOperation( STENCIL_KEEP )
-	render.ClearStencil()
+-- hook.Add( "PostDrawTranslucentRenderables", "PUI.DrawInteractionEffects", function()
+-- 	render.SetStencilWriteMask( 0xFF )
+-- 	render.SetStencilTestMask( 0xFF )
+-- 	render.SetStencilReferenceValue( 0 )
+-- 	render.SetStencilCompareFunction( STENCIL_ALWAYS )
+-- 	render.SetStencilPassOperation( STENCIL_KEEP )
+-- 	render.SetStencilFailOperation( STENCIL_KEEP )
+-- 	render.SetStencilZFailOperation( STENCIL_KEEP )
+-- 	render.ClearStencil()
 
-	-- Enable stencils
-	render.SetStencilEnable( true )
-	-- Force everything to fail
-	render.SetStencilCompareFunction( STENCIL_NEVER )
-	-- Save all the things we don't draw
-	render.SetStencilFailOperation( STENCIL_REPLACE )
+-- 	-- Enable stencils
+-- 	render.SetStencilEnable( true )
+-- 	-- Force everything to fail
+-- 	render.SetStencilCompareFunction( STENCIL_NEVER )
+-- 	-- Save all the things we don't draw
+-- 	render.SetStencilFailOperation( STENCIL_REPLACE )
 
-	-- Set the reference value to 00011100
-	render.SetStencilReferenceValue( 0x1C )
-	-- Set the write mask to 01010101
-	-- Any writes to the stencil buffer will be bitwise ANDed with this mask.
-	-- With our current reference value, the result will be 00010100.
-	render.SetStencilWriteMask( 0x55 )
+-- 	-- Set the reference value to 00011100
+-- 	render.SetStencilReferenceValue( 0x1C )
+-- 	-- Set the write mask to 01010101
+-- 	-- Any writes to the stencil buffer will be bitwise ANDed with this mask.
+-- 	-- With our current reference value, the result will be 00010100.
+-- 	render.SetStencilWriteMask( 0x55 )
 
-	-- Fail to draw our entities.
-	for _, ent in ipairs( ents.FindByClass( "player" ) ) do
-		ent:DrawModel()
-	end
+-- 	-- Fail to draw our entities.
+-- 	for _, ent in ipairs( ents.FindByClass( "player" ) ) do
+-- 		ent:DrawModel()
+-- 	end
 
-	-- Set the test mask to 11110011.
-	-- Any time a pixel is read out of the stencil buffer it will be bitwise ANDed with this mask.
-	render.SetStencilTestMask( 0xF3 )
-	-- Set the reference value to 00011100 & 01010101 & 11110011
-	render.SetStencilReferenceValue( 0x10 )
-	-- Pass if the masked buffer value matches the unmasked reference value
-	-- render.SetStencilCompareFunction( STENCIL_GREATER )
+-- 	-- Set the test mask to 11110011.
+-- 	-- Any time a pixel is read out of the stencil buffer it will be bitwise ANDed with this mask.
+-- 	render.SetStencilTestMask( 0xF3 )
+-- 	-- Set the reference value to 00011100 & 01010101 & 11110011
+-- 	render.SetStencilReferenceValue( 0x10 )
+-- 	-- Pass if the masked buffer value matches the unmasked reference value
+-- 	-- render.SetStencilCompareFunction( STENCIL_GREATER )
 
-	-- Draw our entities
-	-- render.ClearBuffersObeyStencil( 0, 148, 133, 255, false )
-    local plyMat = Material( "prp/ui/temp/glow.png" )
+-- 	-- Draw our entities
+-- 	-- render.ClearBuffersObeyStencil( 0, 148, 133, 255, false )
+--     local plyMat = Material( "prp/ui/temp/glow.png" )
 
-    for _, ent in ipairs( ents.FindByClass( "player" ) ) do
-        if ent == LocalPlayer() then continue end
+--     for _, ent in ipairs( ents.FindByClass( "player" ) ) do
+--         if ent == LocalPlayer() then continue end
 
-        render.SetStencilCompareFunction( STENCIL_GREATER )
-        local targetPos = ent:GetPos() + ent:OBBCenter() -- Center of the target player's bounding box
-        local dir = LocalPlayer():GetPos() - targetPos
-        local ang = dir:Angle()
+--         render.SetStencilCompareFunction( STENCIL_GREATER )
+--         local targetPos = ent:GetPos() + ent:OBBCenter() -- Center of the target player's bounding box
+--         local dir = LocalPlayer():GetPos() - targetPos
+--         local ang = dir:Angle()
 
-        -- Since we're using 3D2D, flip the pitch angle 90 degrees to orient correctly
-        ang:RotateAroundAxis(ang:Right(), -90)
-        ang:RotateAroundAxis(ang:Up(), 90)
-        -- ang.pitch = 0
+--         -- Since we're using 3D2D, flip the pitch angle 90 degrees to orient correctly
+--         ang:RotateAroundAxis(ang:Right(), -90)
+--         ang:RotateAroundAxis(ang:Up(), 90)
+--         -- ang.pitch = 0
 
-        local vPos = ent:GetPos() + ent:OBBCenter()
-        local iScaleFactor = 1 / ent:GetPos():Distance( LocalPlayer():GetPos() )
+--         local vPos = ent:GetPos() + ent:OBBCenter()
+--         local iScaleFactor = 1 / ent:GetPos():Distance( LocalPlayer():GetPos() )
 
-        local iInitialWidth = 40000
-        local iInitialHeight = 75000
+--         local iInitialWidth = 40000
+--         local iInitialHeight = 75000
 
-        local tScreenPos = vPos:ToScreen()
-        local iX = tScreenPos.x - ( iInitialWidth / 2 * iScaleFactor )
-        local iY = tScreenPos.y - ( iInitialHeight / 2 * iScaleFactor )
+--         local tScreenPos = vPos:ToScreen()
+--         local iX = tScreenPos.x - ( iInitialWidth / 2 * iScaleFactor )
+--         local iY = tScreenPos.y - ( iInitialHeight / 2 * iScaleFactor )
 
-        local iWidth = iInitialWidth * iScaleFactor
-        local iHeight = iInitialHeight * iScaleFactor
+--         local iWidth = iInitialWidth * iScaleFactor
+--         local iHeight = iInitialHeight * iScaleFactor
 
-        local iSelectX = tScreenPos.x + ( 10000 * iScaleFactor )
-        local iSelectY = tScreenPos.y - ( 20000 * iScaleFactor )
+--         local iSelectX = tScreenPos.x + ( 10000 * iScaleFactor )
+--         local iSelectY = tScreenPos.y - ( 20000 * iScaleFactor )
 
-        cam.Start2D()
-            render.OverrideDepthEnable( true, false )
+--         cam.Start2D()
+--             render.OverrideDepthEnable( true, false )
 
-            surface.SetMaterial(plyMat)
-            surface.SetDrawColor(255, 255, 255, 64)
+--             surface.SetMaterial(plyMat)
+--             surface.SetDrawColor(255, 255, 255, 64)
 
-            Print( iX, ",\t", iY, ",\t", iWidth, ",\t", iHeight )
+--             Print( iX, ",\t", iY, ",\t", iWidth, ",\t", iHeight )
 
-            surface.DrawTexturedRect(iX, iY, iWidth, iHeight) -- Centered on the player's bounding box
+--             surface.DrawTexturedRect(iX, iY, iWidth, iHeight) -- Centered on the player's bounding box
 
-            -- surface.SetDrawColor( 255, 0, 0, 255 )
-            -- surface.DrawRect( iX, iY, iWidth, iHeight )
+--             -- surface.SetDrawColor( 255, 0, 0, 255 )
+--             -- surface.DrawRect( iX, iY, iWidth, iHeight )
 
-            render.OverrideDepthEnable( false )
-        cam.End2D()
+--             render.OverrideDepthEnable( false )
+--         cam.End2D()
 
-        -- cam.Start3D2D(targetPos, ang, 0.1)
-        --     render.OverrideDepthEnable( true, false )
+--         -- cam.Start3D2D(targetPos, ang, 0.1)
+--         --     render.OverrideDepthEnable( true, false )
 
-        --     surface.SetMaterial(plyMat)
-        --     surface.SetDrawColor(255, 255, 255, 64)
+--         --     surface.SetMaterial(plyMat)
+--         --     surface.SetDrawColor(255, 255, 255, 64)
             
-        --     -- The size of the texture in the 3D world. You might need to adjust these values
-        --     local w, h = 800, 1200
-        --     surface.DrawTexturedRect(-w/2, -h/2, w, h) -- Centered on the player's bounding box
+--         --     -- The size of the texture in the 3D world. You might need to adjust these values
+--         --     local w, h = 800, 1200
+--         --     surface.DrawTexturedRect(-w/2, -h/2, w, h) -- Centered on the player's bounding box
 
-        --     render.OverrideDepthEnable( false )
-        -- cam.End3D2D()
+--         --     render.OverrideDepthEnable( false )
+--         -- cam.End3D2D()
 
-        render.SetStencilCompareFunction( STENCIL_GREATEREQUAL )
+--         render.SetStencilCompareFunction( STENCIL_GREATEREQUAL )
 
-        cam.Start2D()
-            surface.SetDrawColor( PUI.GREEN:Unpack() )
-            surface.DrawRect( iSelectX, iSelectY, 2, 40 )
+--         cam.Start2D()
+--             surface.SetDrawColor( PUI.GREEN:Unpack() )
+--             surface.DrawRect( iSelectX, iSelectY, 2, 40 )
 
-            PUI.StartOverlay()            
-                surface.SetMaterial( Material( "prp/ui/temp/gradient_overlay2_left.png" ) )
-                surface.SetDrawColor( 255, 255, 255, 255 )
-                surface.DrawRect( iSelectX + 2, iSelectY, 200, 40 )
-            PUI.EndOverlay()
+--             PUI.StartOverlay()            
+--                 surface.SetMaterial( Material( "prp/ui/temp/gradient_overlay2_left.png" ) )
+--                 surface.SetDrawColor( 255, 255, 255, 255 )
+--                 surface.DrawRect( iSelectX + 2, iSelectY, 200, 40 )
+--             PUI.EndOverlay()
 
-            surface.SetMaterial( Material( "gui/gradient" ) )
-            surface.SetDrawColor( ColorAlpha( PUI.GREEN, 64 ):Unpack() )
-            surface.DrawTexturedRect( iSelectX + 2, iSelectY, 200, 40 )
+--             surface.SetMaterial( Material( "gui/gradient" ) )
+--             surface.SetDrawColor( ColorAlpha( PUI.GREEN, 64 ):Unpack() )
+--             surface.DrawTexturedRect( iSelectX + 2, iSelectY, 200, 40 )
 
-            surface.SetTextColor( 255, 255, 255, 255 )
-            surface.SetFont( "PRP.UI.Interaction.Option" )
-            surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
-            surface.DrawText( "TRADE" )
+--             surface.SetTextColor( 255, 255, 255, 255 )
+--             surface.SetFont( "PRP.UI.Interaction.Option" )
+--             surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
+--             surface.DrawText( "TRADE" )
 
-            -- 2
+--             -- 2
 
-            iSelectY = iSelectY + 40
+--             iSelectY = iSelectY + 40
 
-            surface.SetDrawColor( 255, 255, 255, 128 )
-            surface.DrawRect( iSelectX, iSelectY, 2, 40 )
+--             surface.SetDrawColor( 255, 255, 255, 128 )
+--             surface.DrawRect( iSelectX, iSelectY, 2, 40 )
 
-            surface.SetTextColor( 255, 255, 255, 128 )
-            surface.SetFont( "PRP.UI.Interaction.Option" )
-            surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
-            surface.DrawText( "COPY STEAMID" )
+--             surface.SetTextColor( 255, 255, 255, 128 )
+--             surface.SetFont( "PRP.UI.Interaction.Option" )
+--             surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
+--             surface.DrawText( "COPY STEAMID" )
 
-            -- 3
+--             -- 3
 
-            iSelectY = iSelectY + 40
+--             iSelectY = iSelectY + 40
 
-            surface.SetDrawColor( 255, 255, 255, 64 )
-            surface.DrawRect( iSelectX, iSelectY, 2, 40 )
+--             surface.SetDrawColor( 255, 255, 255, 64 )
+--             surface.DrawRect( iSelectX, iSelectY, 2, 40 )
 
-            surface.SetTextColor( 255, 255, 255, 64 )
-            surface.SetFont( "PRP.UI.Interaction.Option" )
-            surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
-            surface.DrawText( "PAT DOWN" )
-        cam.End2D()
-    end
+--             surface.SetTextColor( 255, 255, 255, 64 )
+--             surface.SetFont( "PRP.UI.Interaction.Option" )
+--             surface.SetTextPos( iSelectX + 20, iSelectY + ( ( 40 - 20 ) / 2 ) )
+--             surface.DrawText( "PAT DOWN" )
+--         cam.End2D()
+--     end
 
 
-    -- for _, ent in ipairs( ents.FindByClass( "player" ) ) do
-        -- PUI.StartOverlay()
-        --     -- @TODO: Something
-        --     surface.SetMaterial( Material( "effects/flashlight/soft" ) )
-        --     surface.SetDrawColor( 255, 255, 255, 255 )
-        --     surface.DrawTexturedRect( 0, 0, ScrW(), ScrH() )
-        -- PUI.EndOverlay()
-    -- end
+--     -- for _, ent in ipairs( ents.FindByClass( "player" ) ) do
+--         -- PUI.StartOverlay()
+--         --     -- @TODO: Something
+--         --     surface.SetMaterial( Material( "effects/flashlight/soft" ) )
+--         --     surface.SetDrawColor( 255, 255, 255, 255 )
+--         --     surface.DrawTexturedRect( 0, 0, ScrW(), ScrH() )
+--         -- PUI.EndOverlay()
+--     -- end
 
-	-- Let everything render normally again
-	render.SetStencilEnable( false )
-end )
+-- 	-- Let everything render normally again
+-- 	render.SetStencilEnable( false )
+-- end )
