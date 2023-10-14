b94b0f738b277907aff58713cdc574e4d3b7670c
sil <git@sil.dev>
2023-10-13 22:54:48 -0500

    Property Building Physgun Preview

--- plugins/prp_property/hooks/cl_physgun.lua
+++ plugins/prp_property/hooks/cl_physgun.lua
@@ -4,17 +4,23 @@ PRP = PRP or {}
 PRP.Property = PRP.Property or {}
 
 PRP.Property.PhysgunnedEntity = nil
+PRP.Property.PhysgunnedEntityClient = nil
 
 function PLUGIN:PhysgunDrop( pPlayer, eEntity )
     if pPlayer == LocalPlayer() then
         PRP.Prop.PhysgunnedEntity = nil
+
+        SafeRemoveEntity( PRP.Property.PhysgunnedEntityClient )
+
+        PRP.Property.PhysgunnedEntityClient = nil
     end
 
     return
 end
 
-local matGradientUp = Material( "vgui/gradient_up", "alphatest" )
-local matGradientDown = Material( "vgui/gradient_down", "alphatest" )
+local matGradientUp = Material( "vgui/gradient_up" )
+local matGradientDown = Material( "vgui/gradient_down" )
+local matWireframe = Material( "editor/wireframe" )
 local oGradientColor = Color( 164, 32, 32, 128 )
 
 local function DrawGradientBox(pos1, pos2)
@@ -48,6 +54,11 @@ local function DrawGradientBox(pos1, pos2)
     render.DrawQuad(Vector(vPosMin.x, vPosMin.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, upperZ), Vector(vPosMin.x, vPosMin.y, upperZ), oGradientColor )
     render.DrawQuad(Vector(vPosMax.x, vPosMax.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, upperZ), Vector(vPosMax.x, vPosMax.y, upperZ), oGradientColor )
     render.DrawQuad(Vector(vPosMin.x, vPosMin.y, vPosMin.z), Vector(vPosMin.x, vPosMax.y, vPosMin.z), Vector(vPosMin.x, vPosMax.y, upperZ), Vector(vPosMin.x, vPosMin.y, upperZ), oGradientColor )
+
+    -- Bottom
+    render.SetColorMaterial()
+    render.DrawQuad(Vector(vPosMax.x, vPosMax.y, vPosMin.z), Vector(vPosMin.x, vPosMax.y, vPosMin.z), Vector(vPosMin.x, vPosMin.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, vPosMin.z), oGradientColor )
+    render.DrawQuad(Vector(vPosMax.x, vPosMax.y, vPosMin.z), Vector(vPosMin.x, vPosMax.y, vPosMin.z), Vector(vPosMin.x, vPosMin.y, vPosMin.z), Vector(vPosMax.x, vPosMin.y, vPosMin.z), oGradientColor )
 
     render.DepthRange(0, 1)
 end
@@ -96,30 +107,36 @@ function PLUGIN:PostDrawTranslucentRenderables()
     if tSnappedPoints then
         local vSnappedPos, aSnappedAng = PRP.Prop.PhysgunnedEntity:CalcSnappingPos( vTargetPos, vTargetAng, tSnappedPoints )
 
+        if not vSnappedPos:IsEqualTol( PRP.Property.PhysgunnedEntityClient:GetPos(), 1 ) then
+            PRP.Property.PhysgunnedEntityClient:SetPos( vSnappedPos )
+            PRP.Property.PhysgunnedEntityClient:SetAngles( aSnappedAng )
+            PRP.Property.PhysgunnedEntityClient:SetColor( Color( 50, 200, 150, 32 ) )
+        end
+
         -- Draw intersecting entities
         local tFilter = { [tSnappedPoints.theirs.entity:EntIndex()] = true }
         tIntersectingEntities = PRP.Prop.PhysgunnedEntity:CalcIntersect( vSnappedPos, vTargetAng, tFilter )
         for _, eEntity in pairs( tIntersectingEntities ) do
-            render.DrawWireframeBox(
-                eEntity:GetPos(),
-                eEntity:GetAngles(),
-                eEntity:OBBMins(),
-                eEntity:OBBMaxs(),
-                Color( 255, 0, 0 ),
-                false
-            )
+            -- render.DrawWireframeBox(
+            --     eEntity:GetPos(),
+            --     eEntity:GetAngles(),
+            --     eEntity:OBBMins(),
+            --     eEntity:OBBMaxs(),
+            --     Color( 255, 0, 0 ),
+            --     false
+            -- )
         end
 
         local oColor = #tIntersectingEntities > 0 and Color( 255, 0, 0 ) or Color( 50, 200, 150 )
 
-        render.DrawWireframeBox(
-            vSnappedPos,
-            aSnappedAng,
-            vHitBoxMin,
-            vHitBoxMax,
-            oColor,
-            false
-        )
+        -- render.DrawWireframeBox(
+        --     vSnappedPos,
+        --     aSnappedAng,
+        --     vHitBoxMin,
+        --     vHitBoxMax,
+        --     oColor,
+        --     false
+        -- )
 
         for _, tSnappedPoint in pairs( tSnappedPoints ) do
             render.DrawSphere(
@@ -139,16 +156,27 @@ function PLUGIN:PostDrawTranslucentRenderables()
             false
         )
     else
+        -- PRP.Property.PhysgunnedEntityClient:SetNoDraw( true )
+
+        PRP.Property.PhysgunnedEntityClient:SetPos( vTargetPos )
+        PRP.Property.PhysgunnedEntityClient:SetAngles( vTargetAng )
+
+        local iClientEntityAlpha = 32 * math.ease.InQuad( math.Clamp( PRP.Property.PhysgunnedEntityClient:GetPos():Distance( PRP.Prop.PhysgunnedEntity:GetPos() ) / PRP.Prop.PhysgunnedEntity:BoundingRadius(), 0, 1 ) )
+        Print( "what in the fuck?" )
+        Print( PRP.Prop.PhysgunnedEntity:BoundingRadius() )
+        Print( iClientEntityAlpha )
+        PRP.Property.PhysgunnedEntityClient:SetColor4Part( 255, 255, 255, iClientEntityAlpha )
+
         tIntersectingEntities = PRP.Prop.PhysgunnedEntity:CalcIntersect( vTargetPos, vTargetAng )
         for _, eEntity in pairs( tIntersectingEntities ) do
-            render.DrawWireframeBox(
-                eEntity:GetPos(),
-                eEntity:GetAngles(),
-                eEntity:OBBMins(),
-                eEntity:OBBMaxs(),
-                Color( 255, 0, 0 ),
-                false
-            )
+            -- render.DrawWireframeBox(
+            --     eEntity:GetPos(),
+            --     eEntity:GetAngles(),
+            --     eEntity:OBBMins(),
+            --     eEntity:OBBMaxs(),
+            --     Color( 255, 0, 0 ),
+            --     false
+            -- )
         end
     end
 
@@ -165,26 +193,21 @@ function PLUGIN:PostDrawTranslucentRenderables()
         --     true
         -- )
 
-        DrawGradientBox(
-            tZone.pos[1],
-            tZone.pos[2],
-            Color(164, 0, 0),
-            Color(0, 164, 0)
-        )
+        DrawGradientBox( tZone.pos[1], tZone.pos[2] )
     end
 
     local bValidTarget = #tIntersectingEntities == 0 and not bInBlacklistZone
 
     -- Draw the physgunned entity wireframe box.
-    if not tSnappedPoints then
-        render.DrawWireframeBox(
-            vTargetPos,
-            vTargetAng,
-            vHitBoxMin,
-            vHitBoxMax,
-            bValidTarget and Color( 255, 255, 255 ) or Color( 255, 0, 0 ),
-            false
-        )
+    if not tSnappedPoints and not bValidTarget then
+        -- render.DrawWireframeBox(
+        --     vTargetPos,
+        --     vTargetAng,
+        --     vHitBoxMin,
+        --     vHitBoxMax,
+        --     Color( 255, 0, 0 ),
+        --     false
+        -- )
     end
 
     local bAllowed = PRP.Prop.PhysgunnedEntity:CalcTarget()
@@ -198,4 +221,12 @@ net.Receive( "PRP.Property.OnPhysgunPickup", function( iLen )
     -- @TODO: Call our own hook here
 
     PRP.Prop.PhysgunnedEntity = eEntity
+
+    PRP.Property.PhysgunnedEntityClient = ClientsideModel( eEntity:GetModel() )
+    PRP.Property.PhysgunnedEntityClient:SetBodyGroups( eEntity:GetBodyGroups() )
+    PRP.Property.PhysgunnedEntityClient:SetRenderMode( RENDERMODE_TRANSCOLOR )
+    PRP.Property.PhysgunnedEntityClient:SetMaterial( "models/wireframe" )
+    PRP.Property.PhysgunnedEntityClient:SetColor( Color( 255, 255, 255, 16 ) )
+    PRP.Property.PhysgunnedEntityClient:SetPos( eEntity:GetPos() )
+    PRP.Property.PhysgunnedEntityClient:SetAngles( eEntity:GetAngles() )
 end )
