701f64998e9d39cbf7685536662003eecea60710
Nykez <nykezgaming@live.com>
2024-01-18 11:49:27 -0500

    add language strings for tying

--- schema/languages/sh_english.lua
+++ schema/languages/sh_english.lua
@@ -6,4 +6,13 @@ LANGUAGE = {
 	attributeNotFound = "You have specified an invalid skill!",
 	cmdCharSetAttribute = "Sets the level of the specified skill for someone.",
 	cmdCharAddAttribute = "Adds a level to the specified skill for someone.",
+    
+	tying = "Tying...",
+	unTying = "Untying...",
+	isTied = "Tied",
+	fTiedUp = "You have been tied up.",
+	fBeingTied = "You are being tied up.",
+	tiedUp = "They have been tied up.",
+	beingTied = "They are being tied up.",
+	beingUntied = "They are being untied.",
 }
