d550e677051d45882406178f3d851d180e882f13
sil <git@sil.dev>
2024-02-13 11:14:03 -0600

    stub

---
+++ plugins/prp_police/libs/cl_computer.lua
@@ -0,0 +1,19 @@
+PRP = PRP or {}
+PRP.Police = PRP.Police or {}
+PRP.Police.Computer = PRP.Police.Computer or {}
+
+function PRP.Police.Computer.GetCharacters()
+
+end
+
+function PRP.Police.Computer.LookupCharacter( iCharacterID )
+
+end
+
+function PRP.Police.Computer.LookupProperty( sPropertyID )
+
+end
+
+function PRP.Police.Computer.LookupVehicle( sLicensePlate )
+
+end
