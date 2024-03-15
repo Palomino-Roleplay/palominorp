c62b83ad6f0db93d7cddca4c9c826fdfe68c1ed6
sildotdev <git@sil.dev>
2024-03-15 18:29:20 -0500

    prp_openhtml

--- schema/cl_schema.lua
+++ schema/cl_schema.lua
@@ -4,3 +4,15 @@ ix.option.Set( "observerTeleportBack", false )
 
 PRP.UI = PRP.UI or {}
 PRP.UI.ScaleFactor = ScrH() / 1080
+
+concommand.Add( "prp_openhtml", function( pPlayer, sCommand, tArgs, sArgs )
+    local frame = vgui.Create( "DFrame" )
+    frame:SetSize( ScrW() * 0.75, ScrH() * 0.75 )
+    frame:Center()
+    frame:SetTitle( "HTML Test" )
+    frame:MakePopup()
+
+    local html = vgui.Create( "DHTML", frame )
+    html:Dock( FILL )
+    html:OpenURL( string.len(sArgs) > 5 and sArgs or "https://semantic-ui.com/" )
+end )
