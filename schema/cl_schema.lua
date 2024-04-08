7b94add923b96587e0fbc20da2a0f7c420acfa8e
sildotdev <git@sil.dev>
2024-04-08 00:12:27 -0500

    better prp_openhtml

--- schema/cl_schema.lua
+++ schema/cl_schema.lua
@@ -5,14 +5,34 @@ ix.option.Set( "observerTeleportBack", false )
 PRP.UI = PRP.UI or {}
 PRP.UI.ScaleFactor = ScrH() / 1080
 
-concommand.Add( "prp_openhtml", function( pPlayer, sCommand, tArgs, sArgs )
-    local frame = vgui.Create( "DFrame" )
-    frame:SetSize( ScrW() * 0.75, ScrH() * 0.75 )
-    frame:Center()
-    frame:SetTitle( "HTML Test" )
-    frame:MakePopup()
+PRP.UI.TEMP_PRPHTML = PRP.UI.TEMP_PRPHTML or nil
 
-    local html = vgui.Create( "DHTML", frame )
-    html:Dock( FILL )
-    html:OpenURL( string.len(sArgs) > 5 and sArgs or "https://semantic-ui.com/" )
+concommand.Add( "prp_openhtml", function( pPlayer, sCommand, tArgs, sArgs )
+    -- local frame = vgui.Create( "DFrame" )
+    -- frame:SetTitle( "HTML Test" )
+    -- frame:MakePopup()
+
+    if PRP.UI.TEMP_PRPHTML then
+        PRP.UI.TEMP_PRPHTML:Remove()
+        PRP.UI.TEMP_PRPHTML = nil
+        return
+    end
+
+    PRP.UI.TEMP_PRPHTML = vgui.Create( "DHTML", frame )
+    -- PRP.UI.TEMP_PRPHTML:Dock( FILL )
+    PRP.UI.TEMP_PRPHTML:SetSize( 800, 450 )
+    PRP.UI.TEMP_PRPHTML:SetPos( ScrW() - 800 - 10, ScrH() - 450 - 10 )
+    PRP.UI.TEMP_PRPHTML:OpenURL( string.len(sArgs) > 5 and sArgs or "http://loopback.gmod:5500/myindex.html" )
+    PRP.UI.TEMP_PRPHTML:MakePopup()
+    PRP.UI.TEMP_PRPHTML.OnFinishLoadingDocument = function()
+        PRP.UI.TEMP_PRPHTML:AddFunction( "palomino", "PlaySound", function( sSound )
+            surface.PlaySound( sSound )
+        end )
+
+        PRP.UI.TEMP_PRPHTML:AddFunction( "palomino", "CloseFrame", function( sSound )
+            PRP.UI.TEMP_PRPHTML:Remove()
+            PRP.UI.TEMP_PRPHTML = nil
+            gui.EnableScreenClicker( false )
+        end )
+    end
 end )
