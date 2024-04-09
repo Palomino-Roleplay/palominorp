c461a78f683cb274c9c5744b6b626485ca5cb897
sildotdev <git@sil.dev>
2024-04-09 18:23:10 -0500

    quest plugin, stub NPCs

---
+++ plugins/prp_quests/npcs/sh_diner.lua
@@ -0,0 +1,4 @@
+NPC.name = "Matilda"
+NPC.model = "models/mossman.mdl"
+NPC.description = "Hii sugar, what can I get ya?"
+NPC.robbable = false
---
+++ plugins/prp_quests/npcs/sh_hardware.lua
@@ -0,0 +1,16 @@
+NPC.name = "Matilda"
+NPC.model = "models/mossman.mdl"
+NPC.description = "gag"
+NPC.robbable = true
+
+-- This is how you get a crowbar?
+
+-- Robbery: You have to point gun *near* him, but not directly *at* him.
+    -- If you point gun directly at him, he will get scared and shut down.
+
+-- "Ohh!! Welcome! Please, please, take a look around.\nWe've got blue paint, red paint, green paint, yellow paint, and even more paint!\n"
+    -- "I'd like to buy something?"
+-- "Buy?? No!! Why would I sell you my paint? It's mine!"
+    -- "But, this is a store, right?"
+-- "What?! A store? This is a museum of my hardware collection!"
+    -- "...right."
