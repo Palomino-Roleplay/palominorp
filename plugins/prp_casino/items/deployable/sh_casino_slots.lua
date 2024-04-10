7914f21ab091213992004b40ce2dd32310fad0bc
sildotdev <git@sil.dev>
2024-04-10 01:31:54 -0500

    Added casino items (stub)

---
+++ plugins/prp_casino/items/deployable/sh_casino_slots.lua
@@ -0,0 +1,9 @@
+ITEM.name = "Slot Machine"
+ITEM.model = "models/freeman/owain_slotmachine.mdl"
+ITEM.width = 1
+ITEM.height = 2
+ITEM.description = ""
+ITEM.category = "Casino"
+ITEM.entClass = "pcasino_slot_machine"
+
+-- @TODO: On spawn function
