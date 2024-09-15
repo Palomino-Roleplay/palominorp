fdf00b67bebe13c64dcc70468cc6ba7391ff7b67
sildotdev <git@sil.dev>
2024-09-15 09:32:16 -0500

    Put prp_openhtml in a DFrame

--- schema/cl_schema.lua
+++ schema/cl_schema.lua
@@ -18,18 +18,20 @@ concommand.Add( "prp_openhtml", function( pPlayer, sCommand, tArgs, sArgs )
         return
     end
 
-    PRP.UI.TEMP_PRPHTML = vgui.Create( "DHTML", frame )
+    PRP.UI.TEMP_PRPHTML = vgui.Create( "DFrame", frame )
     -- PRP.UI.TEMP_PRPHTML:Dock( FILL )
     PRP.UI.TEMP_PRPHTML:SetSize( 800, 450 )
     PRP.UI.TEMP_PRPHTML:SetPos( ScrW() - 800 - 10, ScrH() - 450 - 10 )
-    PRP.UI.TEMP_PRPHTML:OpenURL( string.len(sArgs) > 5 and sArgs or "http://loopback.gmod:5500/myindex.html" )
+    local dHTMLPanel = vgui.Create( "DHTML", PRP.UI.TEMP_PRPHTML )
+    dHTMLPanel:Dock( FILL )
+    dHTMLPanel:OpenURL( string.len(sArgs) > 5 and sArgs or "http://loopback.gmod:5500/myindex.html" )
     PRP.UI.TEMP_PRPHTML:MakePopup()
-    PRP.UI.TEMP_PRPHTML.OnFinishLoadingDocument = function()
-        PRP.UI.TEMP_PRPHTML:AddFunction( "palomino", "PlaySound", function( sSound )
+    dHTMLPanel.OnFinishLoadingDocument = function()
+        dHTMLPanel:AddFunction( "palomino", "PlaySound", function( sSound )
             surface.PlaySound( sSound )
         end )
 
-        PRP.UI.TEMP_PRPHTML:AddFunction( "palomino", "CloseFrame", function( sSound )
+        dHTMLPanel:AddFunction( "palomino", "CloseFrame", function( sSound )
             PRP.UI.TEMP_PRPHTML:Remove()
             PRP.UI.TEMP_PRPHTML = nil
             gui.EnableScreenClicker( false )
